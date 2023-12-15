d4daf3f34dfb93cbd8b986b60485cb64ed9530f3
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-12-14 23:52:02 -0300

    Crear constructor

--- cypress/support/POM/Login.Page.js
+++ cypress/support/POM/Login.Page.js
@@ -1,6 +1,12 @@
 class Login{
 
     get = {
-
+        endpoint: ()=> cy.visit("/auth/login"),
+        usernameInput: () => cy.get('[name = "username"]'),
+        passwordInput: () => cy.get('[name = "password"]'),
+        submitButton: () => cy.get('[type = "submit"]'),
+        forgotLink: () => cy.get('[class*= "login-forgot"] p'),
     }
+
+
 }
