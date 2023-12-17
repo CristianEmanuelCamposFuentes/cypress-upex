c7eac7de3a786f558b5cabc02d2d53a2c0c5d086
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-12-17 19:04:02 -0300

    Definir clase Login.Page.js

--- cypress/e2e/Tests/login.cy.js
+++ cypress/e2e/Tests/login.cy.js
@@ -1,1 +1,21 @@
-import { login} from "../../support/POM/Login.Page";
+//import { login} from "../../support/POM/Login.Page"; // ES6 module
+
+const { login} = require("../../support/POM/Login.Page");
+
+const {authLogin, dashboardIndex} = Cypress.env('endpoint');
+// Desestructurar el objeto del usuario
+const {username, password} = Cypress.env('AdminUser');
+describe("user logs in", () => {
+    beforeEach(() => {
+       cy.visit('/')
+       cy.url().should('contain',authLogin)
+    });
+
+    it('validate login succesfully', () => {
+        login.enterUsername(username);
+        login.enterPassword(password);
+        login.submitLogin();
+
+        cy.url().should('contain',dashboardIndex)
+    })
+})
